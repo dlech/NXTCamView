cad6e8a543d3cb250ffc17bee87df7ae32801504
tingeypa <tingeypa@487e7d13-5337-0410-b72c-a7988805691d>
2007-09-02 08:11:49 +0000

    Add simple web page for sourceforge
    git-svn-id: svn://svn.code.sf.net/p/nxtcamview/code/trunk@10 487e7d13-5337-0410-b72c-a7988805691d

---
+++ web/htdocs/index.php
@@ -0,0 +1,88 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="en-gb">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>NXTCamView</title>
+<meta name="keywords" content="Lego NXTCam mindsensors">
+<meta name="Microsoft Theme" content="none, default">
+</head>
+
+<body>
+
+<p><b><font face="Verdana" size="6"><font color="#FF6600">NXT</font><font color="#0000FF">CamView</font></font></b><font face="Verdana" size="2"> 
+A manager for you NXTCam camera sensor</font></p>
+<hr noshade color="#0000FF" size="9">
+<table border="0" width="944" height="337">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td>&nbsp;</td>
+		<td height="311">
+		<table cellpadding="0" cellspacing="0" border="0" width="100%" height="100%">
+			<!-- MSCellFormattingTableID="1" -->
+			<tr>
+				<td bgcolor="#0000FF" nowrap>
+				<!-- MSCellFormattingType="header" -->
+				&nbsp;</td>
+				<td bgcolor="#0000FF" height="20">
+				<span style="background-color: #0000FF">
+				<img alt="" src="MsoPnl_Cnr_tr_173.gif" width="20" height="20"></span></td>
+			</tr>
+			<tr>
+				<td bgcolor="#0000FF" colspan="2" height="100%">
+				<!-- MSCellFormattingType="content" -->
+				<table border="0" width="100%" height="100%" cellpadding="3" cellspacing="0">
+					<tr>
+						<td width="100%" height="100%" valign="top">
+						<font face="Verdana" size="2" color="#FFFFFF">
+						<a href="http://sourceforge.net/project/screenshots.php?group_id=203058">
+						<font color="#FFFFFF">Screenshots</font></a></font><p>
+						<font face="Verdana" size="2">
+						<a href="http://nxtcamview.wiki.sourceforge.net/FAQ">
+						<font color="#FFFFFF">FAQ</font></a></font></p>
+						<p><a href="http://nxtcamview.wiki.sourceforge.net/FAQ">
+						<font face="Verdana" size="2" color="#FFFFFF">Wiki</font></a></p>
+						<p><font face="Verdana" size="2" color="#FFFFFF">
+						<a href="http://sourceforge.net/project/showfiles.php?group_id=203058">
+						<font color="#FFFFFF">Download</font></a></font></p>
+						<p><font face="Verdana" size="2" color="#FFFFFF">
+						<a title="Send any queries or requests here" href="mailto:NXTCamView@tingeys.com?subject=NXTCamView">
+						<font color="#FFFFFF">Contact</font></a></font></td>
+					</tr>
+				</table>
+				</td>
+			</tr>
+			<tr>
+				<td nowrap bgcolor="#0000FF" width="100%">
+				<!-- MSCellFormattingType="footer" -->
+				&nbsp;</td>
+				<td height="20" width="20">
+				<span style="background-color: #0000FF">
+				<img alt="" src="MsoPnl_Cnr_br_175.gif" width="20" height="20"></span></td>
+			</tr>
+		</table>
+		</td>
+		<td height="313" valign="top">
+<p><b><font color="#3333CC" size="4" face="Verdana">home page</font></b></p>
+<p><font face="Verdana" size="2">NXTCamView configures and controls the NXTCam 
+produced by
+<a class="wiki_link_ext" rel="nofollow" href="http://mindsensors.com/">
+mindsensors</a>. NXTCam is a camera sensor that provides real-time vision 
+recognition to NXT Lego robots. NXTCam uses simple colour recognition to detect 
+objects in its view.</font></p>
+&nbsp;</td>
+	</tr>
+	<tr>
+		<td width="6"></td>
+		<td width="95"></td>
+		<td height="18" width="829"></td>
+	</tr>
+</table>
+<div style="position: absolute; width: 210px; height: 61px; z-index: 1; left: 734px; top: 9px" id="layer1">
+	<font face="Verdana"><a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=203058&amp;type=5" width="210" height="62" border="0" alt="SourceForge.net Logo" /></a></font></div>
+<p>
+<font face="Verdana">
+<a href="http://sourceforge.net/donate/index.php?group_id=203058"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /></a> </font><font face="Verdana" size="2">Any donations towards Lego hardware are much appreciated!</font></p>
+</body>
+
+</html>
