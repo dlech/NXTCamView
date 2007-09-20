fa4ab8942e49d8cbcb82265f086023c9553eb477
tingeypa <tingeypa@487e7d13-5337-0410-b72c-a7988805691d>
2007-09-20 22:34:55 +0000

    - Made Nant build requirement clearer - Removed dependency on MSBuild tasks
    git-svn-id: svn://svn.code.sf.net/p/nxtcamview/code/trunk@25 487e7d13-5337-0410-b72c-a7988805691d

--- build/setNantPath.cmd
+++ build/setNantPath.cmd
@@ -1,1 +1,27 @@
-set path=%path%;%~p0..\tools\nant\nant-0.85\bin
+echo off
+prompt $
+
+set NANTPATH=%~p0..\tools\nant
+
+echo.
+echo -----------------------
+echo Check NAnt install
+echo -----------------------
+if not exist "%NANTPATH%\nant-0.85\bin\nant.exe" goto nant_error
+
+echo * NAnt is installed OK
+echo.
+
+set path=%path%;%NANTPATH%\nant-0.85\bin
+goto done
+
+:nant_error
+echo * Problem: NAnt not installed
+echo.
+echo This build requires that NAnt is installed.  You can download NAnt v0.85 
+echo from Sourceforge.net and install it to directory - 
+echo   %NANTPATH%
+echo.
+
+:done
+prompt
